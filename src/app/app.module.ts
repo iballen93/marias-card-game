6fbf4b9c49b37b8a712008a66286f4400164fd6a
iballen93 <iballen93@gmail.com>
2025-03-01 01:05:38 -0700

    more more more more more more package updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { PointsComponent } from './points/points.component';
 import { CardPileComponent } from './components/card-pile/card-pile.component';
 import { TurnInfoComponent } from './components/turn-info/turn-info.component';
 import { HandComponent } from 'src/app/components/hand/hand.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CardPileComponent,
-    TurnInfoComponent,
-    HandComponent,
-    PointsComponent
-  ],
-  imports: [
-    BrowserModule, HttpClientModule,
-  ],
-  providers: [HttpClientModule],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CardPileComponent,
+        TurnInfoComponent,
+        HandComponent,
+        PointsComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [HttpClientModule, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
--- src/app/deck-of-cards-service.service.ts
+++ src/app/deck-of-cards-service.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Card} from './components/card-pile/card-pile.component';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
