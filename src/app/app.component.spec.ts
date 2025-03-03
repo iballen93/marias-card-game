887303e821da7829da9e9dbfac7a79015f3c860f
iballen93 <iballen93@gmail.com>
2025-03-02 18:39:09 -0700

    Revert "more more more more more more package updates"
    This reverts commit 6fbf4b9c49b37b8a712008a66286f4400164fd6a.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import {HttpClient, HttpHandler} from '@angular/common/http';
 
 describe('AppComponent', () => {
   let service: AppComponent;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { PointsComponent } from './points/points.component';
 import { CardPileComponent } from './components/card-pile/card-pile.component';
 import { TurnInfoComponent } from './components/turn-info/turn-info.component';
 import { HandComponent } from 'src/app/components/hand/hand.component';
 
-@NgModule({ declarations: [
-        AppComponent,
-        CardPileComponent,
-        TurnInfoComponent,
-        HandComponent,
-        PointsComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule], providers: [HttpClientModule, provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardPileComponent,
+    TurnInfoComponent,
+    HandComponent,
+    PointsComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+  ],
+  providers: [HttpClientModule],
+  bootstrap: [AppComponent]
+})
 export class AppModule { }
